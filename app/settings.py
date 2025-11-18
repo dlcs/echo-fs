aaeebdafb2b9f8b911a8ad3153caf5efe4b61969
Donald Gray <donald.gray@digirati.com>
2025-11-18 10:52:47 +0000

    Remove use of distutils for parsing booleans
    distutils is now deprecated, use custom method based on deprecated
    equivalent

--- app/settings.py
+++ app/settings.py
@@ -1,7 +1,21 @@
 import os
-import distutils.util
 
-DEBUG = bool(distutils.util.strtobool(os.environ.get("DEBUG", default="False")))
+# See: https://github.com/python/cpython/blob/v3.11.2/Lib/distutils/util.py#L308
+def strtobool(val) -> bool:
+    """Convert a string representation of truth to true (1) or false (0).
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
+    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
+    'val' is anything else.
+    """
+    val = val.lower()
+    if val in ('y', 'yes', 't', 'true', 'on', '1'):
+        return True
+    elif val in ('n', 'no', 'f', 'false', 'off', '0'):
+        return False
+    else:
+        raise ValueError("invalid truth value %r" % (val,))
+
+DEBUG = strtobool(os.environ.get("DEBUG", default="False"))
 
 REDIS_HOST = os.environ.get('ECHO_REDIS_HOST')
 REDIS_PORT = int(os.environ.get('ECHO_REDIS_PORT'))
@@ -21,5 +35,5 @@ ERROR_QUEUE = os.environ.get('ECHO_ERROR_QUEUE')
 SCAVENGER_SLEEP_SECONDS = int(os.environ.get('ECHO_SCAVENGER_SLEEP_SECONDS', default="30"))
 SCAVENGER_MIN_AGE_SECONDS = int(os.environ.get('ECHO_SCAVENGER_MIN_AGE_SECONDS', default="0"))
 
-POPULATE_LOOP = bool(distutils.util.strtobool(os.environ.get("ECHO_POPULATE_LOOP", default="False")))
+POPULATE_LOOP = strtobool(os.environ.get("ECHO_POPULATE_LOOP", default="False"))
 POPULATE_SLEEP_SECONDS = int(os.environ.get("ECHO_POPULATE_SLEEP_SECONDS", default="300"))
